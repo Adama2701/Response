f4c30c98a6381104758a8eb18290e3d345b87d84
Filip Gusak <figus12@student.sdu.dk>
2017-02-26 16:49:54 +0100

    Lavede start activity og lidt ændring i gui.

---
+++ app/src/main/java/com/example/adama/response/Startactivity.java
@@ -0,0 +1,29 @@
+package com.example.adama.response;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Startactivity extends AppCompatActivity {
+    Button proceed;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_startactivity);
+
+        proceed = (Button) findViewById(R.id.proceed);
+
+
+        proceed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent fp = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(fp);
+            }
+        });
+    }
+
+
+}
