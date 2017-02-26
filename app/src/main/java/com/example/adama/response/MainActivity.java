405747bf919b55b33875dadc79032598449ada8e
Adama <adama camara>
2017-02-26 14:40:59 +0100

    added most of functionality

--- app/src/main/java/com/example/adama/response/MainActivity.java
+++ app/src/main/java/com/example/adama/response/MainActivity.java
@@ -1,13 +1,102 @@
 package com.example.adama.response;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button1;
+    Button button2;
+    Button button3;
+    Button button4;
+    ImageView imageView2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+
+        button1 = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
+        button4 = (Button) findViewById(R.id.button4);
+        imageView2 = (ImageView) findViewById(R.id.imageView2);
+
+
+        imageView2.setVisibility(View.INVISIBLE);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!imageView2.isShown()) {
+
+                    imageView2.setVisibility(View.VISIBLE);
+
+                }else{
+                    imageView2.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!imageView2.isShown()) {
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    imageView2.setVisibility(View.VISIBLE);
+
+                }else{
+                    imageView2.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!imageView2.isShown()) {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    imageView2.setVisibility(View.VISIBLE);
+
+                }else{
+                    imageView2.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!imageView2.isShown()) {
+                    try {
+                        Thread.sleep(3000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    imageView2.setVisibility(View.VISIBLE);
+
+                }else{
+                    imageView2.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
     }
 }
