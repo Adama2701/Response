21305ca160c5df97926656ae1c989921e97e4182
Filip <gusak93@gmai.comgit config --global user.email gusak93@gmail.com>
2017-04-05 12:50:13 +0200

    Gui for den nye

--- app/src/main/java/com/example/adama/response/FoodActivity.java
+++ app/src/main/java/com/example/adama/response/FoodActivity.java
@@ -1,39 +1,33 @@
 package com.example.adama.response;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.Spinner;
 
 public class FoodActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
     Spinner month;
     Spinner day;
+    FloatingActionButton plusbutton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food);
 
-        final Button amount1 = (Button) findViewById(R.id.amount1);
-        amount1.setTag(1);
-        amount1.setText("1");
-        amount1.setOnClickListener(new View.OnClickListener() {
+        plusbutton = (FloatingActionButton) findViewById(R.id.plusbutton);
+        plusbutton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                final int status =(Integer) v.getTag();
-                if(status == 1) {
-                    amount1.setText("2");
-                    v.setTag(0);
-                } else {
-                    amount1.setText("1");
-                    v.setTag(1);
-
-                }
+            public void onClick(View view) {
+                Intent jud = new Intent(getApplicationContext(), PlusButtonActivity.class);
+                startActivity(jud);
             }
         });
+
         month = (Spinner) findViewById(R.id.month);
         day = (Spinner) findViewById(R.id.day);
 
---
+++ app/src/main/java/com/example/adama/response/PlusButtonActivity.java
@@ -0,0 +1,44 @@
+package com.example.adama.response;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class PlusButtonActivity extends AppCompatActivity {
+Button complete;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plus_button);
+
+        complete = (Button) findViewById(R.id.complete);
+        complete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent xa = new Intent(getApplicationContext(), FoodActivity.class);
+                startActivity(xa);
+            }
+        });
+
+        final Button amount = (Button) findViewById(R.id.amount);
+        amount.setTag(1);
+        amount.setText("1");
+        amount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final int status = (Integer) v.getTag();
+                if (status == 1) {
+                    amount.setText("2");
+                    v.setTag(0);
+                } else {
+                    amount.setText("1");
+                    v.setTag(1);
+
+                }
+            }
+
+        });
+    }}
+
